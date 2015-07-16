9d60ad44821c6e78a6e7aad4c98dfc7244044880
Vincent Labatut <vince.labatut@gmail.com>
2015-07-16 10:49:35 +0200

    fixed comments

--- src/common/misc.R
+++ src/common/misc.R
@@ -6,13 +6,13 @@
 
 
 #######################################################
-# Processes an adjacenct table, possibly making it
+# Processes an adjacency table, possibly making it
 # symmetrical.
 #
 # mat: a 2-column matrix (=links).
 # sym: whether the adjacency matrix should be
 # 	   symmetrical or not.
-# levels: the levels fetch to the table function.
+# levels: the levels fetched to the table function.
 #######################################################
 process.adjacency <- function(mat, sym, levels)
 {	res <- as.matrix(table(factor(mat[,1],levels=levels),factor(mat[,2],levels=levels)))
